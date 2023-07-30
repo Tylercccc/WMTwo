d337d4304cbbee618edbb5e21b1ff2026773334b
Tyler <tcauthe1@gmail.com>
2023-07-29 20:31:40 -0400

    new unity input manager

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 75.0f;
+    public GameObject player;
+
+    private WizardActions controls;
+
+    public void Awake()
     {
-        
+        controls = new WizardActions();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //transform.LookAt(player.transform);
+        Vector3 v3 = new Vector3(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0.0f);
+        transform.RotateAround(player.transform.position,v3,Time.deltaTime * speed);
+    }
+    public void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    public void OnDisable()
+    {
+        controls.Disable();
     }
 }
