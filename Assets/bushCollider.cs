c9e8643cac48cb49d0570ea2c4c4c34fc3a6d9cd
Tyler <tcauthe1@gmail.com>
2023-11-05 17:31:39 -0500

    refactor bush collision + leaves

--- Assets/bushCollider.cs
+++ Assets/bushCollider.cs
@@ -5,35 +5,39 @@ using UnityEngine;
 public class bushCollider : MonoBehaviour
 {
     public Vector3 offset;
+
     private void Update()
     {
-        //transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
         Shader.SetGlobalVector("_BushPosition", transform.position + offset);
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Bush")
         {
-            LeanTween.cancel(other.gameObject);
-            Material bushMat = other.GetComponent<MeshRenderer>().material;
-            Vector3 currentSquash = bushMat.GetVector("_Squash");
-            LeanTween.value(other.gameObject, currentSquash, new Vector3(2.25f, -1.5f, 2.25f), 0.75f).setEase(LeanTweenType.easeOutBack).setOnUpdate((Vector3 val) =>
-                 {
-                     bushMat.SetVector("_Squash", val);
-                 });
+            BushAnimate(other, new Vector3(2.25f, -1.5f, 2.25f), 0.75f, LeanTweenType.easeOutBack);
         }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "Bush")
         {
-            LeanTween.cancel(other.gameObject);
-            Material bushMat = other.GetComponent<MeshRenderer>().material;
-            Vector3 currentSquash = bushMat.GetVector("_Squash");
-            LeanTween.value(other.gameObject, currentSquash, new Vector3(0, 0, 0), 2f).setEase(LeanTweenType.easeOutElastic).setOnUpdate((Vector3 val) =>
-            {
-                bushMat.SetVector("_Squash", val);
-            });
+            BushAnimate(other, new Vector3(0, 0, 0), 2f, LeanTweenType.easeOutElastic);
         }
+    }
+    private void BushAnimate(Collider other, Vector3 squashAmt, float squashTime, LeanTweenType ltType )
+    {
+        LeanTween.cancel(other.gameObject);
+        Material bushMat = other.GetComponent<MeshRenderer>().material;
+        Vector3 currentSquash = bushMat.GetVector("_Squash");
+        LeanTween.value(other.gameObject, currentSquash, squashAmt, squashTime).setEase(ltType).setOnUpdate((Vector3 val) =>
+        {
+            bushMat.SetVector("_Squash", val);
+        });
     }
 }
